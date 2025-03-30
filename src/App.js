da7ef5f0c4507e700dd4c79b395a2cc89a46fde9
Naiyyar <naiyyar.abbas512013@gmail.com>
2025-03-30 16:09:16 +0530

    rendering between pages on link click

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import Directory from "./components/directory/directory-components.jsx"
+import { Routes, Route } from "react-router-dom";
+
+import Home from './routes/home/home.component.jsx'
+import Navigation from "./routes/navigation/navigation.component.jsx"
+
+const Shop = () => {
+  return(
+    <h1>This is Shop page!</h1>
+  )
+}
   
 const App = () => {
-  return <Directory />
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  )
 }
 export default App;
--- src/components/directory/directory-components.jsx
+++ src/components/directory/directory-components.jsx
@@ -2,35 +2,39 @@ import "./directory.styles.scss"
 import CategoryItem from "../category-item/category-item.jsx"
 
 const Directory = () => {
-    const categories = [
-        {
-          "id": 1,
-          "title": 'Womens',
-          "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-        },{
-          "id": 2,
-          "title": 'Men',
-          "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-        },{
-          "id": 3,
-          "title": 'Hats',
-          "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-        },{
-          "id": 4,
-          "title": 'Jackets',
-          "imageUrl": "https://i.ibb.co/cvpntL1/jackets.png"
-        }
-    ]
+  const categories = [
+    {
+      "id": 1,
+      "title": 'Womens',
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },{
+      "id": 2,
+      "title": 'Men',
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    },{
+      "id": 3,
+      "title": 'Hats',
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },{
+      "id": 4,
+      "title": 'Jackets',
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },{
+      "id": 5,
+      "title": 'Sneakers',
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    }
+  ]
 
-    return (
-        <div className="directory-container">
-        {
-          categories.map((category) => (
-            <CategoryItem key={category.id} category={category} />
-          ))
-        }  
-        </div>
-    )
+  return (
+    <div className="directory-container">
+    {
+      categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))
+    }  
+    </div>
+  )
 }
 
 export default Directory

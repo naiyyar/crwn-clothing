7bd36798472d0daaf9f1576c8f6e42a9c9b61d2f
Naiyyar <naiyyar.abbas512013@gmail.com>
2025-03-30 16:08:43 +0530

    routes/navigation components

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,12 @@
+import Directory from '../../components/directory/directory-components.jsx'
+
+const Home = () => {
+    return(
+        <div>
+            <Directory />
+            
+        </div>
+    )
+}
+
+export default Home
